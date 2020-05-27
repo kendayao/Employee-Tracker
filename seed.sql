12b2e94dda2d935b24c539bba3d9830458c78ec5
Kaeneth Dayao <kendayao@gmail.com>
2020-05-27 14:16:54 -0700

    added database schema and create tables schema for department, role, employee, manager

---
+++ seed.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE database employee_db;
+
+USE employee_db;
+
+CREATE TABLE deparment (
+    id  INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL (10,2),
+    department_id INT,
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE manager (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    department_id INT,
+    PRIMARY KEY(id)
+)
