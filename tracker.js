b0f466ab61effbd94309bc0a37d99a97ce570380
Kaeneth Dayao <kendayao@gmail.com>
2020-05-27 17:49:48 -0700

    setup mysql connection

--- tracker.js
+++ tracker.js
@@ -3,11 +3,8 @@ var inquirer=require("inquirer")
 
 var connection = mysql.createConnection({
   host: "localhost",
-
   port: 3306,
-
   user: "root",
-
   password: "",
   database: "employee_db"
 });
@@ -15,4 +12,5 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  connection.end();
 });
